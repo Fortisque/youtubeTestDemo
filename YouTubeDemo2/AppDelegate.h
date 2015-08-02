a773aa77c2c344d6c9919a71ed3f8337b0259f24
Kevin Casey <kacasey@berkeley.edu>
2015-08-02 07:56:40 -0700

    Initial Commit

---
+++ YouTubeDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouTubeDemo2
+//
+//  Created by Kevin Casey on 8/2/15.
+//  Copyright (c) 2015 Kevin Casey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
